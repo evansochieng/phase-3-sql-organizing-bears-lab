b4d4e914151791b8cfcc396beb1bf28e6a3bc134
Evans Ochieng <evansochieng121@gmail.com>
2022-10-26 11:12:19 +0300

    organize bears using queries

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+--Populate bears table with data
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 7, "M", "black", "warm", TRUE);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 3, "M", "grey", "fiery", 1);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 5, "F", "white", "gentle", TRUE);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 5, "M", "grey", "spirited", FALSE);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 9, "F", "grey", "cheerful", 0);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 7, "M", "black", "demure", TRUE);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 1, "F", "white", "calm", FALSE);
+
+INSERT INTO
+bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, NULL, "F", "black", "fussy", TRUE);
